3800186a38037b3743f7c47b9256e2474c4dc479
Khanya <sibulelekupelo@gmail.com>
2022-09-03 17:45:26 +0200

    Add Brute force solution without optimisation

--- 2Pointer-Technique/index.js
+++ 2Pointer-Technique/index.js
@@ -1,26 +1,29 @@
-const arr = [1, 3, 7, 9, 2]
+const numbers = [1, 3, 7, 9, 2]
 const target = 11;
+// Give function a proper name
+const findTwoSum = (numbers, target) => {
 
-const x = () => {
-    // array to return items
-    const answer = []
+    // This block code processes if solution exists
+    for (let p1 = 0; p1 < numbers.length; p1++) {
 
+        // calculate number to find
+        const numberToFind = target - numbers[p1];
 
-    for (let i = 0; i < arr.length; i++) {
-        let numberToFind = target - arr[i];
-        console.log(`p1: ${i} number to find: ${numberToFind}`)
-
-        for (let j = 0; j < arr.length; j++) {
-            console.log(`P1: ${i} P2: ${j} number to find: ${numberToFind}`)
-            let p2 = j+ 1;
-            if(numberToFind===arr[p2]){
-                answer.push(arr[p2])
+        // To prevent comparing the p1's numberToFind to its self, p2 must = p1+1
+        // The initial value of p2 is dependent on where p1 is
+        for (let p2 = p1 + 1; p2 < numbers.length; p2++) {
+            console.log(`P1: ${p1} P2: ${p2} number to find: ${numberToFind}`)
+            if (numberToFind === numbers[p2]) {
+                return [p1, p2]
+                // answer.push(arr[p2])
             }
         }
 
     }
-    console.log(answer)
+
+    // There is no existing solution on the given array
+    return null;
 }
 
-x();
+findTwoSum(numbers, target);
 
