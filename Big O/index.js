d7d160a2efaa108719341f2dfd58b66da5f69721
Khanya <dev@kupelo.co.za>
2022-11-02 18:45:14 +0200

    introducing O(n^2)

--- Big O/index.js
+++ Big O/index.js
@@ -1,28 +1,31 @@
 console.log("Good Code is readable and scalable");
-// const performance = require('perf_hooks').performance;
+const performance = require('perf_hooks').performance;
 
-// let everyone = ["darla", "dory", "bruce", "marlin", "gill", "bloat", "hank", "nemo", "nigel", "squirt"];
-// let nemo = ["nemo"];
-// let large = new Array(1000000).fill("Nemo");
-// /**
-//  * Big O depends on the number of inputs 
-//  * @param {*} arr 
-//  */
-// const findNemo = (arr) => {
-//     // let t0 = performance.now();
-//     for (let i = 0; i < large.length; i++) {
-//         if (arr[i] === 'nemo') {
-//             console.log("We found Nemo!");
-//         }
-//     }
-//     // let t1 = performance.now();
-//     // console.log("It took ", + (t1 - t0), " milliseconds to find Nemo");
+let everyone = ["darla", "dory", "bruce", "marlin", "gill", "bloat", "hank", "nemo", "nigel", "squirt"];
+let nemo = ["nemo"];
+let large = new Array(1000000).fill("Nemo");
+/**
+ * Big O depends on the number of inputs 
+ * @param {*} arr 
+ */
+const findNemo = (arr) => {
+    let t0 = performance.now();
+    for (let i = 0; i < large.length; i++) {
+        if (arr[i] === 'nemo') {
+            console.log("We found Nemo!");
+        }
+    }
+    let t1 = performance.now();
+    console.log("It took ", + (t1 - t0), " milliseconds to find Nemo");
 
-// };
+};
 
-// findNemo(everyone); // O(n) ---> it takes linear time to find nemo
+console.log("------------ O(n) --------------------------------");
+findNemo(everyone); // O(n) ---> it takes linear time to find nemo
 
 
+
+console.log("------------ O(1) --------------------------------");
 
 /**
  * Measuring Big O
@@ -40,3 +43,19 @@ const logFirstTwoBoxes = (boxes) => {
 logFirstTwoBoxes(boxes); // O(2)
 
 
+console.log("------------ O(n^2) --------------------------------");
+
+/**
+ * Log all pairs of array
+ */
+let pairs = [1, 2, 3, 4, 5];
+let logAllPairs = function (pairs) {
+    for (let i = 0; i < pairs.length; i++) {
+        for (let j = 0; j < pairs.length; j++) {
+            console.log(pairs[i], pairs[i]);
+
+        }
+    }
+};
+
+logAllPairs(pairs);
