ded844cba8054c03a5d45ce635582250e29c6e45
Khanya <dev@kupelo.co.za>
2022-11-10 14:02:47 +0200

    google exam question

--- Data-Structures/Hash-Tables/interview.js
+++ Data-Structures/Hash-Tables/interview.js
@@ -1,6 +1,6 @@
 
 // Input sorted array of numbers
-const scores = [2, 5, 1, 2, 3, 4, 5, 6, 7,];
+const scores = [2, 5, 5, 2, 3, 4, 5, 6, 7,];
 const scores_2 = [2, 1, 1, 2, 3, 4, 5, 6, 7,];
 const scores_3 = [2, 3, 4, 5, 6, 7,];
 
