3966d851de6c08fe79393decac26dfea4ac78da3
Khanya <dev@kupelo.co.za>
2022-11-13 00:18:40 +0200

    Custom linked list and functions

--- Data-Structures/LinkedLists/Custom.js
+++ Data-Structures/LinkedLists/Custom.js
@@ -13,21 +13,42 @@ class LinkedList {
         this.length = 1;
     }
 
+    /**
+     * Adds to the end of the linkedList
+     * @param {*} value Is the value we want to add
+     * @returns LinkedList
+     */
     append = (value) => {
         const newNode = {
             value: value,
             next: null
         };
+
         this.tail.next = newNode;
         this.tail = newNode;
+        this.length++;
 
-
-
+        return this;
     };
+
+    prepend = (value) => {
+        const newNode = {
+            value: value,
+            next: null
+        };
+
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+
+        return this;
+    };
+
 }
 
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
-console.log(myLinkedList);
+myLinkedList.prepend(1);
+console.log(myLinkedList.head);
